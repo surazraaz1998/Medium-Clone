1c188738e6de9686a200eacbeea6caafb6af2c9f
Suraj Raj <srraaz1998@gmail.com>
2022-01-21 14:29:30 +0530

    Finished Version 1 of Medium Clone

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="flex w-full mx-auto  p-2 ">
+      <p className="text-gray-400 w-full text-center">
+        Made with 💗 by Suraj Raj<a className="no-underline font-bold" href="https://github.com/surazraaz1998">{" "}@anonymous__raaz</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
